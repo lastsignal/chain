e6bdc451d5a728b5d087862423fc55845044eb84
Dan Daneshgar <dan_daneshgar@dell.com>
2018-01-22 09:19:52 -0600

    Modify IoC supported unit tests

--- UnitTests/D_UsingIoc.cs
+++ UnitTests/D_UsingIoc.cs
@@ -9,7 +9,9 @@ namespace UnitTests
     [TestFixture]
     public class D_UsingIoc
     {
-        private IContainer _container;
+        private IContainer _container; // a chain of responsibility
+        private IContainer _sequenceControlledContainer56;
+        private IContainer _sequenceControlledContainer65;
 
         [SetUp]
         public void SetUp()
@@ -29,6 +31,40 @@ namespace UnitTests
                         .Singleton();
                 }
             );
+
+            _sequenceControlledContainer56 = new Container(_ =>
+                {
+                    _.Scan(s =>
+                    {
+                        s.TheCallingAssembly();
+                        s.AddAllTypesOf<ILinkMarker>();
+                    });
+
+                    _.For<ILinkMarker>().Add<L5>();
+                    _.For<ILinkMarker>().Add<L6>();
+                    
+                    _.For<IChain<Message>>()
+                        .Use<L5L6Chain>()
+                        .Singleton();
+                }
+            );
+
+            _sequenceControlledContainer65 = new Container(_ =>
+                {
+                    _.Scan(s =>
+                    {
+                        s.TheCallingAssembly();
+                        s.AddAllTypesOf<ILinkMarker>();
+                    });
+
+                    _.For<ILinkMarker>().Add<L6>();
+                    _.For<ILinkMarker>().Add<L5>();
+                    
+                    _.For<IChain<Message>>()
+                        .Use<L5L6Chain>()
+                        .Singleton();
+                }
+            );
         }
 
         [Test]
@@ -42,8 +78,38 @@ namespace UnitTests
 
             Results.Count.ShouldEqual(2);
 
+            Results.Index.ShouldContain("L5: M1");
+            Results.Index.ShouldContain("L6: M1");
+        }
+
+        [Test]
+        public void An_ioc_made_chain_should_should_make_the_5_6_sequence()
+        {
+            var message = new Message {Name = "M1"};
+
+            var c = _sequenceControlledContainer56.GetInstance<IChain<Message>>();
+
+            c.ExecuteAll(message);
+
+            Results.Count.ShouldEqual(2);
+
             Results.Index[0].ShouldEqual("L5: M1");
             Results.Index[1].ShouldEqual("L6: M1");
+        }
+
+        [Test]
+        public void An_ioc_made_chain_should_should_make_the_6_5_sequence()
+        {
+            var message = new Message {Name = "M1"};
+
+            var c = _sequenceControlledContainer65.GetInstance<IChain<Message>>();
+
+            c.ExecuteAll(message);
+
+            Results.Count.ShouldEqual(2);
+
+            Results.Index[0].ShouldEqual("L6: M1");
+            Results.Index[1].ShouldEqual("L5: M1");
         }
     }
 }
